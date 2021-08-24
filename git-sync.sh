1cc399c11b8139f28f44c56713d7f669b1057f5f
Devendra Tewari <devendra.tewari@gmail.com>
2021-08-24 08:08:19 -0300

    specify branch in GIT_BRANCH environment variable

--- git-sync.sh
+++ git-sync.sh
@@ -1,8 +1,12 @@
 git remote add upstream https://git.yoctoproject.org/git/poky
 git fetch upstream
-git checkout master
+git_branch=$GIT_BRANCH
+if [ "$git_branch" = "" ]; then
+    git_branch="master"
+fi
+git checkout $git_branch
 if [ $? -eq 0 ]; then
-    git rebase upstream/master
+    git rebase upstream/$git_branch
     if [ $? -eq 0 ]; then
         git push -f
     fi
